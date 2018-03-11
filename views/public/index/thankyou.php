5f0d06b315bfae6ffe4cfba6ec919b5160cb55f6
Daniel Berthereau <Daniel.github@Berthereau.net>
2018-03-12 00:00:00 +0100

    Fixed form.

--- views/public/index/thankyou.php
+++ views/public/index/thankyou.php
@@ -1,4 +1,11 @@
-<?php echo head(); ?>
+<?php
+$pageTitle = __('Contact Us');
+echo head(array(
+    'title' => $pageTitle,
+    'bodyclass' => 'contact-us',
+    'bodyid' => 'contact-us',
+));
+?>
 <div id="primary">
     <?php echo flash(); ?>
     <h1><?php echo html_escape(get_option('simple_contact_page_thankyou_title')); // Not HTML ?></h1>
--- views/public/index/write.php
+++ views/public/index/write.php
@@ -1,10 +1,33 @@
-<?php echo head(); ?>
+<?php
+$pageTitle = __('Contact Us');
+echo head(array(
+    'title' => $pageTitle,
+    'bodyclass' => 'contact-us',
+    'bodyid' => 'contact-us',
+));
+?>
 <div id="primary">
     <?php echo flash(); ?>
     <h1><?php echo html_escape(get_option('simple_contact_page_contact_title')); ?></h1>
     <div id="form-instructions">
         <?php echo get_option('simple_contact_page_contact_text'); // HTML ?>
     </div>
-    <?php echo $this->simpleContactForm(); ?>
+    <?php
+    $options = array(
+        'form_attributes' => array(
+            'id' => 'contact-form',
+            'method' => 'post',
+            'enctype' => 'multipart/form-data',
+            'accept-charset' => 'utf-8',
+            'class' => 'form-horizontal',
+        ),
+        'name' => $name,
+        'email' => $email,
+        'message' => $message,
+        'captcha' => $captcha,
+        'path' => 'contact',
+    );
+    echo $this->simpleContactForm($options);
+    ?>
 </div>
 <?php echo foot();
